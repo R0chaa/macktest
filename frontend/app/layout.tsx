188aa8baeb57ca50b4475c4e706dc655997af107
Lucas Rocha <lucas.rcha04@gmail.com>
2025-11-08 21:51:33 -0300

    feat: initialize frontend with Next.js, TypeScript, and Tailwind

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MackTest",
+  description: "Desafio Técnico Mackenzie",
+  authors: [{ name: "Lucas Rocha :)", url: "https://github.com/R0chaa" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
